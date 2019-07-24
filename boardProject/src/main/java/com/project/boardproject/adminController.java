8525d31a199f2caba761823016a527bd99a79482
Mintchoco91 <boy0221@hanmail.net>
2019-07-24 16:07:19 +0900

    NODB_VER

--- boardProject/src/main/java/com/project/boardproject/adminController.java
+++ boardProject/src/main/java/com/project/boardproject/adminController.java
@@ -1,4 +1,4 @@
-package com.project.boardproject.admin;
+package com.project.boardproject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
