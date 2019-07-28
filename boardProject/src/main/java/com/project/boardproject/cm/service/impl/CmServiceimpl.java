282c0a87831d3c239b590cc9136ff6065d2fbbc0
Mintchoco91 <boy0221@hanmail.net>
2019-07-28 19:02:59 +0900

    ADD_MariaDB

--- boardProject/src/main/java/com/project/boardproject/cm/service/impl/CmServiceimpl.java
+++ boardProject/src/main/java/com/project/boardproject/cm/service/impl/CmServiceimpl.java
@@ -16,7 +16,7 @@ public class CmServiceimpl implements CmService {
 	
 	public String sampleData() {
 		String sampleResult = "";
-		sampleResult = "DB에서 가져온값";
+		sampleResult = "impl에서 가져온값임. DB에서 가져오도록 수정할것";
 		
 		//DB연결 후 주석제거할것
 		//sampleResult = cmdao.sampleData();
