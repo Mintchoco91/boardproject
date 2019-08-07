893be489affc5b2041756e2d2d5e650d8acd604c
Mintchoco91 <boy0221@hanmail.net>
2019-08-08 00:24:25 +0900

    파일정리

--- boardProject/src/main/java/com/project/boardproject/cm/web/CmController.java
+++ boardProject/src/main/java/com/project/boardproject/cm/web/CmController.java
@@ -1,7 +1,6 @@
 package com.project.boardproject.cm.web;
 
-import javax.inject.Inject;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,7 @@ import com.project.boardproject.cm.service.CmService;
 @Controller
 public class CmController {
 	
-	@Inject
+	@Autowired
 	private CmService cmservice;
 	
 	@RequestMapping("index")
