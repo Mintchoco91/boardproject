1dc759681b48f7192d9df9a6148c39c2cd78079e
Mintchoco91 <boy0221@hanmail.net>
2019-08-27 21:29:10 +0900

    merge 후 오류처리

--- boardProject/src/main/java/com/project/boardproject/cm/web/CmController.java
+++ boardProject/src/main/java/com/project/boardproject/cm/web/CmController.java
@@ -10,17 +10,12 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.project.boardproject.cm.service.BoardList;
 import com.project.boardproject.cm.service.BoardVO;
 import com.project.boardproject.cm.service.CmService;
+import com.project.boardproject.mm.service.MemberVO;
 
 /*
  * 파일명 : customController.java
